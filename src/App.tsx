9fb97b680b82559aa90e08f9f64cfb8cb8e0cd7b
dasun shyaminda <dasunshyaminda0518@gmail.com>
2024-08-27 20:43:20 +0530

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css';
 import {Route, Routes} from 'react-router-dom';
+import AuthLogin from "./Layouts/AuthLogin";
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<AuthLogin />} />
       </Routes>
     </div>
   );
---
+++ src/Layouts/AuthLogin.tsx
@@ -0,0 +1,12 @@
+import LoginPage from "../Pages/LoginPage";
+
+const AuthLogin = () => {
+    return (
+        <div>
+            Auth Login Page
+            <LoginPage />
+        </div>
+    )
+}
+
+export default AuthLogin
