04945dd2869042ef52632f5b27aae0a91e0149b8
dasun shyaminda <dasunshyaminda0518@gmail.com>
2024-08-28 18:07:43 +0530

    commit

--- src/Layouts/AuthLogin.tsx
+++ src/Layouts/AuthLogin.tsx
@@ -1,10 +1,15 @@
+import "../Comman/css/styles.css"
 import LoginPage from "../Pages/LoginPage";
+import logo from "../Assests/Images/logo.png"
 
 const AuthLogin = () => {
     return (
-        <div>
-            Auth Login Page
-            <LoginPage />
+        <div className="main-outer-container">
+            <div className="login-container">
+                <div className="login-header">
+                    <img className="logo" src={logo} alt=""/>
+                </div>
+            </div>
         </div>
     )
 }
