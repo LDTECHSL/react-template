64978cacfd84517e0ecf9d86ce48af669e065f29
dasun shyaminda <dasunshyaminda0518@gmail.com>
2024-09-04 21:05:10 +0530

    commit

--- src/Layouts/Dashboard.tsx
+++ src/Layouts/Dashboard.tsx
@@ -1,56 +1,60 @@
-import * as React from 'react';
-import { styled, Theme, CSSObject } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import MuiDrawer from '@mui/material/Drawer';
-import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import CssBaseline from '@mui/material/CssBaseline';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import Collapse from '@mui/material/Collapse';
-import ExpandLess from '@mui/icons-material/ExpandLess';
-import ExpandMore from '@mui/icons-material/ExpandMore';
-import { ReactElement } from 'react';
-import { useNavigate } from 'react-router-dom';
+import * as React from "react";
+import { styled, Theme, CSSObject } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import MuiDrawer from "@mui/material/Drawer";
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Collapse from "@mui/material/Collapse";
+import ExpandLess from "@mui/icons-material/ExpandLess";
+import ExpandMore from "@mui/icons-material/ExpandMore";
+import { ReactElement } from "react";
+import { useNavigate } from "react-router-dom";
 import "../Common/css/styles.css";
-import { AccountCircleIcon, HomeOutlinedIcon, MenuIcon } from '../Assets/Icons/Icons';
-import TurnedInNotOutlined from '@mui/icons-material/TurnedInNotOutlined';
-import Footer from '../Components/Footer';
-import text from "../Assets/Text/Text.json"
+import {
+  AccountCircleIcon,
+  HomeOutlinedIcon,
+  MenuIcon,
+} from "../Assets/Icons/Icons";
+import TurnedInNotOutlined from "@mui/icons-material/TurnedInNotOutlined";
+import Footer from "../Components/Footer";
+import text from "../Assets/Text/Text.json";
 import logo from "../Assets/Images/logo.png";
 
 const drawerWidth = 250;
 
 const openedMixin = (theme: Theme): CSSObject => ({
   width: drawerWidth,
-  transition: theme.transitions.create('width', {
+  transition: theme.transitions.create("width", {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.enteringScreen,
   }),
-  overflowX: 'hidden',
+  overflowX: "hidden",
 });
 
 const closedMixin = (theme: Theme): CSSObject => ({
-  transition: theme.transitions.create('width', {
+  transition: theme.transitions.create("width", {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
   }),
-  overflowX: 'hidden',
+  overflowX: "hidden",
   width: `calc(${theme.spacing(7)} + 1px)`,
-  [theme.breakpoints.up('sm')]: {
+  [theme.breakpoints.up("sm")]: {
     width: `calc(${theme.spacing(8)} + 1px)`,
   },
 });
 
-const DrawerHeader = styled('div')(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'flex-end',
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
   padding: theme.spacing(0, 1),
   ...theme.mixins.toolbar,
 }));
@@ -60,10 +64,10 @@ interface AppBarProps extends MuiAppBarProps {
 }
 
 const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== 'open',
+  shouldForwardProp: (prop) => prop !== "open",
 })<AppBarProps>(({ theme }) => ({
   zIndex: theme.zIndex.drawer + 1,
-  transition: theme.transitions.create(['width', 'margin'], {
+  transition: theme.transitions.create(["width", "margin"], {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
   }),
@@ -73,7 +77,7 @@ const AppBar = styled(MuiAppBar, {
       style: {
         marginLeft: drawerWidth,
         width: `calc(100% - ${drawerWidth}px)`,
-        transition: theme.transitions.create(['width', 'margin'], {
+        transition: theme.transitions.create(["width", "margin"], {
           easing: theme.transitions.easing.sharp,
           duration: theme.transitions.duration.enteringScreen,
         }),
@@ -82,30 +86,30 @@ const AppBar = styled(MuiAppBar, {
   ],
 }));
 
-const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
-  ({ theme }) => ({
-    width: drawerWidth,
-    flexShrink: 0,
-    whiteSpace: 'nowrap',
-    boxSizing: 'border-box',
-    variants: [
-      {
-        props: ({ open }) => open,
-        style: {
-          ...openedMixin(theme),
-          '& .MuiDrawer-paper': openedMixin(theme),
-        },
+const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme }) => ({
+  width: drawerWidth,
+  flexShrink: 0,
+  whiteSpace: "nowrap",
+  boxSizing: "border-box",
+  variants: [
+    {
+      props: ({ open }) => open,
+      style: {
+        ...openedMixin(theme),
+        "& .MuiDrawer-paper": openedMixin(theme),
       },
-      {
-        props: ({ open }) => !open,
-        style: {
-          ...closedMixin(theme),
-          '& .MuiDrawer-paper': closedMixin(theme),
-        },
+    },
+    {
+      props: ({ open }) => !open,
+      style: {
+        ...closedMixin(theme),
+        "& .MuiDrawer-paper": closedMixin(theme),
       },
-    ],
-  }),
-);
+    },
+  ],
+}));
 
 interface Props {
   children: ReactElement;
@@ -124,7 +128,7 @@ export default function Dashboard({ children }: Readonly<Props>) {
 
   const handleSampleClick = () => {
     navigate("/dashboard/sample");
-    setOpenSample2(false)
+    setOpenSample2(false);
     setActive(1);
     setActiveChild(null); // Reset child item selection
   };
@@ -143,7 +147,7 @@ export default function Dashboard({ children }: Readonly<Props>) {
   };
 
   return (
-    <Box sx={{ display: 'flex' }}>
+    <Box sx={{ display: "flex" }}>
       <CssBaseline />
       <AppBar position="fixed" open={open}>
         <Toolbar className="toolbar">
@@ -160,103 +164,120 @@ export default function Dashboard({ children }: Readonly<Props>) {
             <MenuIcon />
           </IconButton>
           <div className="toolbar-inner">
-            <AccountCircleIcon className='icon-user' />
+            <AccountCircleIcon className="icon-user" />
             <span className="username">Hi, Dasun Shyaminda</span>
           </div>
         </Toolbar>
       </AppBar>
       <Drawer variant="permanent" open={open}>
-      <div className="drawer-header">
+        <div className="drawer-header">
           <img className="drawer-logo" src={logo} alt="" />
           <span className="login-header-text">{text.authLoginPage.header}</span>
         </div>
         <List>
-  {['Sample', 'Sample2'].map((text, index) => (
-    <ListItem key={text} disablePadding sx={{ display: 'block' }}>
-      <ListItemButton
-        onClick={index === 0 ? handleSampleClick : handleSample2Click}
-        sx={[
-          {
-            minHeight: 48,
-            px: 2.5,
-            backgroundColor: index === (active - 1) ? '#e6f4ff' : 'transparent',
-            color: index === (active - 1) ? "#1677ff" : "black",
-            borderRight: index === (active - 1) ? "2px solid #1677ff" : "none",
-            '&:hover': {
-              backgroundColor: index === (active - 1) ? '#e6f4ff' : 'transparent',
-            },
-          },
-          open
-            ? {
-                justifyContent: 'initial',
-              }
-            : {
-                justifyContent: 'center',
-              },
-        ]}
-      >
-        <ListItemIcon
-          sx={[
-            {
-              minWidth: 0,
-              justifyContent: 'center',
-              color: index === (active - 1) ? "#1677ff" : "black",
-            },
-            open
-              ? {
-                  mr: 3,
-                }
-              : {
-                  mr: 'auto',
-                },
-          ]}
-        >
-          {index === 0 ? <HomeOutlinedIcon style={{ fontSize: '20px' }} /> : <TurnedInNotOutlined style={{ fontSize: '20px' }} />}
-        </ListItemIcon>
-        <ListItemText
-          primary={text}
-          primaryTypographyProps={{ fontSize: '14px' }} // Adjusting the font size for the text
-          sx={[
-            open
-              ? {
-                  opacity: 1,
-                }
-              : {
-                  opacity: 0,
-                },
-          ]}
-        />
-        {index === 1 && open ? (openSample2 ? <ExpandLess /> : <ExpandMore />) : null}
-      </ListItemButton>
-      {index === 1 ? (
-        <Collapse in={openSample2} timeout="auto" unmountOnExit>
-          <List component="div" disablePadding>
-            <ListItem sx={{ pl: 6 }}>
-              <ListItemButton onClick={() => handleChildClick(1)}>
+          {["Sample", "Sample2"].map((text, index) => (
+            <ListItem key={text} disablePadding sx={{ display: "block" }}>
+              <ListItemButton
+                onClick={index === 0 ? handleSampleClick : handleSample2Click}
+                sx={[
+                  {
+                    minHeight: 48,
+                    px: 2.5,
+                    backgroundColor:
+                      index === active - 1 ? "#e6f4ff" : "transparent",
+                    color: index === active - 1 ? "#1677ff" : "black",
+                    borderRight:
+                      index === active - 1 ? "2px solid #1677ff" : "none",
+                    "&:hover": {
+                      backgroundColor:
+                        index === active - 1 ? "#e6f4ff" : "transparent",
+                    },
+                  },
+                  open
+                    ? {
+                        justifyContent: "initial",
+                      }
+                    : {
+                        justifyContent: "center",
+                      },
+                ]}
+              >
+                <ListItemIcon
+                  sx={[
+                    {
+                      minWidth: 0,
+                      justifyContent: "center",
+                      color: index === active - 1 ? "#1677ff" : "black",
+                    },
+                    open
+                      ? {
+                          mr: 3,
+                        }
+                      : {
+                          mr: "auto",
+                        },
+                  ]}
+                >
+                  {index === 0 ? (
+                    <HomeOutlinedIcon style={{ fontSize: "20px" }} />
+                  ) : (
+                    <TurnedInNotOutlined style={{ fontSize: "20px" }} />
+                  )}
+                </ListItemIcon>
                 <ListItemText
-                  primary="Child Item 1"
-                  primaryTypographyProps={{ fontSize: '14px' }} // Adjust font size for child items
-                  sx={{ color: activeChild === 1 ? "#1677ff" : "black" }}
+                  primary={text}
+                  primaryTypographyProps={{ fontSize: "14px" }} // Adjusting the font size for the text
+                  sx={[
+                    open
+                      ? {
+                          opacity: 1,
+                        }
+                      : {
+                          opacity: 0,
+                        },
+                  ]}
                 />
+                {index === 1 && open ? (
+                  openSample2 ? (
+                    <ExpandLess />
+                  ) : (
+                    <ExpandMore />
+                  )
+                ) : null}
               </ListItemButton>
+              {index === 1 ? (
+                <Collapse in={openSample2} timeout="auto" unmountOnExit>
+                  <List component="div" disablePadding>
+                    <ListItem sx={{ pl: 6 }}>
+                      <ListItemButton onClick={() => handleChildClick(1)}>
+                        <ListItemText
+                          primary="Child Item 1"
+                          primaryTypographyProps={{ fontSize: "14px" }} // Adjust font size for child items
+                          sx={{
+                            color: activeChild === 1 ? "#1677ff" : "black",
+                          }}
+                        />
+                      </ListItemButton>
+                    </ListItem>
+                    <ListItem sx={{ pl: 6 }}>
+                      <ListItemButton onClick={() => handleChildClick(2)}>
+                        <ListItemText
+                          primary="Child Item 2"
+                          primaryTypographyProps={{ fontSize: "14px" }} // Adjust font size for child items
+                          sx={{
+                            color: activeChild === 2 ? "#1677ff" : "black",
+                          }}
+                        />
+                      </ListItemButton>
+                    </ListItem>
+                    {/* Add more child items as needed */}
+                  </List>
+                </Collapse>
+              ) : null}
             </ListItem>
-            <ListItem sx={{ pl: 6 }}>
-              <ListItemButton onClick={() => handleChildClick(2)}>
-                <ListItemText
-                  primary="Child Item 2"
-                  primaryTypographyProps={{ fontSize: '14px' }} // Adjust font size for child items
-                  sx={{ color: activeChild === 2 ? "#1677ff" : "black" }}
-                />
-              </ListItemButton>
-            </ListItem>
-            {/* Add more child items as needed */}
-          </List>
-        </Collapse>
-      ) : null}
-    </ListItem>
-  ))}
-</List>
-        
+          ))}
+        </List>
+
         <Divider />
       </Drawer>
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
