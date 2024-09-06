bc2ad02c3a4c6ce871e8d26688c53a32677f2b34
Dasun Shyaminda <dasunshyaminda0518@gmail.com>
2024-09-06 13:47:58 +0530

    commit

---
+++ src/Components/Dialogbox.tsx
@@ -0,0 +1,48 @@
+import "../Common/css/dialogbox.css";
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+
+interface Dialogboxprops {
+  open: boolean;
+  title: string;
+  content: string;
+  agreeButtonText: string;
+  disagreeButtonText: string;
+  onAgree: () => void;
+  onDisagree: () => void;
+  onClose: () => void;
+}
+
+export default function Dialogbox(props: Dialogboxprops) {
+  return (
+    <Dialog
+      open={props.open}
+      onClose={props.onClose}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+      sx={{ "& .MuiPaper-root": { border: "2px solid black" } }}
+    >
+      <DialogTitle id="alert-dialog-title">{props.title}</DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-description">{props.content}</DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={() => { props.onDisagree(); props.onClose();}}
+            style={{ color: 'green', backgroundColor: 'white'}}
+            >
+            {props.disagreeButtonText}
+        </Button>
+        <Button onClick={() => { props.onAgree(); props.onClose();}} autoFocus
+            style={{ color: 'green', backgroundColor: 'white'}}
+            >
+            {props.agreeButtonText}
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
