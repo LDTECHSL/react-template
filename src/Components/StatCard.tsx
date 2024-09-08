b7a5c1c010baa346bff6467c6d08603cc4dc2318
dasun shyaminda <dasunshyaminda0518@gmail.com>
2024-09-08 21:22:59 +0530

    commit

--- src/Components/StatCard.tsx
+++ src/Components/StatCard.tsx
@@ -6,6 +6,13 @@ export default function Stat_card(){
             <div className="stat-header">
                 <span className="stat-title">Total Page Views</span>
             </div>
+            <div className="stat-content">
+                <span className="stat-total">102,485</span>
+            </div>
+            <div className="space-bar"></div>
+            <div className="stat-content">
+                <span className="stat-footer">You made an extra 20,000 this week</span>
+            </div>
         </div>
     )
 }
